453aa94a76ad9de691590af4927cf3e8d42c5112
mayurir674-debug <mayurir674@gmail.com>
2026-02-18 23:48:18 +0530

    Update AppConfigure class file

--- src/main/java/com/desktop/util/config/AppConfig.java
+++ src/main/java/com/desktop/util/config/AppConfig.java
@@ -1,8 +1,14 @@
 package com.desktop.util.config;
 
+import com.desktop.controller.BookingController;
 import com.desktop.controller.DeskController;
+import com.desktop.dao.BookingDao;
 import com.desktop.dao.DeskDao;
+import com.desktop.dao.PaymentDao;
+import com.desktop.dao.implementation.JdbcBookingDao;
 import com.desktop.dao.implementation.JdbcDeskDao;
+import com.desktop.dao.implementation.JdbcPaymentDao;
+import com.desktop.service.BookingService;
 import com.desktop.service.DeskService;
 
 public class AppConfig {
@@ -11,6 +17,14 @@ public class AppConfig {
 	        DeskService deskService = new DeskService(deskDao);
 	        return new DeskController(deskService);
 
+	        
 }
+	 public BookingController bookingController() {
+	        DeskDao deskDao = new JdbcDeskDao();
+	        BookingDao bookingDao = new JdbcBookingDao();
+	        PaymentDao paymentDao = new JdbcPaymentDao();
+	        BookingService bookingService = new BookingService(deskDao, bookingDao, paymentDao);
+	        return new BookingController(bookingService);
+	    }
 }	 
 	 
