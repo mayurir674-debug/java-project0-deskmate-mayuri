a704f1b69ac80150efcd8c2bfe79722411331dfa
mayurir674-debug <mayurir674@gmail.com>
2026-02-19 00:41:30 +0530

    Update AppConfig file with ReportControlller

--- src/main/java/com/desktop/util/config/AppConfig.java
+++ src/main/java/com/desktop/util/config/AppConfig.java
@@ -2,14 +2,18 @@ package com.desktop.util.config;
 
 import com.desktop.controller.BookingController;
 import com.desktop.controller.DeskController;
+import com.desktop.controller.ReportController;
 import com.desktop.dao.BookingDao;
 import com.desktop.dao.DeskDao;
 import com.desktop.dao.PaymentDao;
+import com.desktop.dao.ReportDao;
 import com.desktop.dao.implementation.JdbcBookingDao;
 import com.desktop.dao.implementation.JdbcDeskDao;
 import com.desktop.dao.implementation.JdbcPaymentDao;
+import com.desktop.dao.implementation.JdbcReportDao;
 import com.desktop.service.BookingService;
 import com.desktop.service.DeskService;
+import com.desktop.service.ReportService;
 
 public class AppConfig {
 	 public DeskController deskController() {
@@ -25,6 +29,11 @@ public class AppConfig {
 	        PaymentDao paymentDao = new JdbcPaymentDao();
 	        BookingService bookingService = new BookingService(deskDao, bookingDao, paymentDao);
 	        return new BookingController(bookingService);
+	    }
+	  public ReportController reportController() {
+	        ReportDao reportDao = new JdbcReportDao();
+	        ReportService reportService = new ReportService(reportDao);
+	        return new ReportController(reportService);
 	    }
 }	 
 	 
