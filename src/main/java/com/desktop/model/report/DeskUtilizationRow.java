7d4aefa79fac66203b06500e30490405c81775a1
mayurir674-debug <mayurir674@gmail.com>
2026-02-19 00:27:35 +0530

    Add report DTOs and ReportDao contract

---
+++ src/main/java/com/desktop/model/report/DeskUtilizationRow.java
@@ -0,0 +1,14 @@
+package com.desktop.model.report;
+
+public class DeskUtilizationRow {
+    private final String deskCode;
+    private final long paidBookingsCount;
+
+    public DeskUtilizationRow(String deskCode, long paidBookingsCount) {
+        this.deskCode = deskCode;
+        this.paidBookingsCount = paidBookingsCount;
+    }
+
+    public String getDeskCode() { return deskCode; }
+    public long getPaidBookingsCount() { return paidBookingsCount; }
+}
