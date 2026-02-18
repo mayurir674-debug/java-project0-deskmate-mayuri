81daf77c7883e180335ce501d6c4dffd515dcce6
mayurir674-debug <mayurir674@gmail.com>
2026-02-18 14:15:33 +0530

    Add testMain class file

---
+++ src/main/java/com/desktop/TestMain.java
@@ -0,0 +1,16 @@
+package com.desktop;
+
+import java.sql.Connection;
+
+import com.desktop.util.DbConnectionFactory;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection con =DbConnectionFactory.getConnection();
+		
+
+	}
+
+}
